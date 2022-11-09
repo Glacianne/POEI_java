0ad4cb2dcfcd41b1edf080c4b45c189929392528
Anne-Isabelle <anne.isabelle.rene@gmail.com>
2022-11-09 16:22:32 +0100

    amélioration du fichier School en ajoutant le principe de classe

--- Semaine_3_java/SchoolAir.java
+++ Semaine_3_java/SchoolAir.java
@@ -41,16 +41,18 @@ class Principal extends Employee {
 
 class Teacher extends Employee {
     protected String lesson;
+    protected Grade grade;
 
-    public Teacher(String name, String lesson) {
+    public Teacher(String name, String lesson, Grade grade) {
         super(name, "professeur", 2000);
         this.lesson = lesson;
+        this.grade = grade;
     }
 
     @Override
     public void whoAmI() {
         super.whoAmI();
-        System.out.print(String.format(" de %s.", lesson));
+        System.out.print(String.format(" de %s", lesson));
     }
 }
 
@@ -63,7 +65,7 @@ class Volunteer extends Person {
     }
 
     public void whoAmI() {
-        System.out.print(String.format("Je m'appelle %s, je suis bénévole, je suis %s.", name, job));
+        System.out.print(String.format("Je m'appelle %s, je suis bénévole, je suis %s", name, job));
     }
 }
 
@@ -79,22 +81,77 @@ class Cleaning extends Volunteer {
     }
 }
 
-class Student extends Person {
-    protected String grad;
+class Grade {
+    public String name;
 
-    public Student(String name, String grad) {
+    public Grade(String name) {
+        this.name = name;
+    }
+
+    public void getPersonsByGrade() {
+        System.out.println(String.format("Les professeurs de la classe de %s : ", this.name));
+        for (Teacher teacher : SchoolAir.getTeacherList()) {
+            if (this.name.equals(teacher.grade.name)) {
+                System.out.println("- " + teacher.name + " professeur de " + teacher.lesson);
+            }
+        }
+        System.out.println(String.format("Les élèves de la classe de %s : ", this.name));
+        for (Student student : SchoolAir.getStudentsList()) {
+            if (this.name.equals(student.grade.name)) {
+                System.out.println("- " + student.name);
+            }
+        }
+    }
+}
+
+class Mark {
+    Double number;
+
+    // constructeur
+    public Mark(Double number) {
+        this.number = number;
+    }
+
+}
+
+class Student extends Person {
+    protected Grade grade;
+    ArrayList<Mark> marks = new ArrayList<>();
+
+    public Student(String name, Grade grade) {
         super(name);
-        this.grad = grad;
+        this.grade = grade;
     }
 
     public void whoAmI() {
-        System.out.print(String.format("Je m'appelle %s, je suis étudiant en classe de %s.", name, grad));
+        System.out.print(String.format("Je m'appelle %s, je suis étudiant(e)", name));
+    }
+
+    public void addMark(Mark mark) {
+        marks.add(mark);
+    }
+
+    public ArrayList<Double> getAllMarks() {
+        ArrayList<Double> allMarks = new ArrayList<>();
+        for (Mark m : marks) {
+            allMarks.add(m.number);
+        }
+        return allMarks;
+    }
+
+    public Double getAverage() {
+        ArrayList<Double> allMarks = this.getAllMarks();
+        Double total = 0.00;
+        for (Double number : allMarks) {
+            total += number;
+        }
+        return Double.isNaN((total / marks.size())) ? 0.0 : total / marks.size();
     }
 
 }
 
 public class SchoolAir {
-    ArrayList<Person> persons = new ArrayList<>();
+    public static ArrayList<Person> persons = new ArrayList<>();
 
     public Person getPersonByName(String name) {
         for (Person person : persons) {
@@ -111,21 +168,49 @@ public class SchoolAir {
     public void ringBell() {
         for (Person person : persons) {
             person.whoAmI();
-            System.out.println("");
+            System.out.println(".");
         }
+    }
+
+    public static ArrayList<Student> getStudentsList() {
+        ArrayList<Student> students = new ArrayList<Student>();
+        for (Person person : persons) {
+            if (person.getClass().getSimpleName().equals("Student")) {
+                students.add((Student) person);
+            }
+        }
+        return students;
+    }
+
+    public static ArrayList<Teacher> getTeacherList() {
+        ArrayList<Teacher> teachers = new ArrayList<Teacher>();
+        for (Person person : persons) {
+            if (person.getClass().getSimpleName().equals("Teacher")) {
+                teachers.add((Teacher) person);
+            }
+        }
+        return teachers;
     }
 
     public static void main(String[] args) {
 
         SchoolAir school1 = new SchoolAir();
 
+        Grade quatrieme = new Grade("quatrième");
+        Grade cinquieme = new Grade("cinquième");
         school1.addPerson(new Principal("Jacques"));
-        school1.addPerson(new Teacher("ALbert", "mathématiques"));
+        school1.addPerson(new Teacher("ALbert", "mathématiques", quatrieme));
         school1.addPerson(new Kitchen("Bernard"));
         school1.addPerson(new Cleaning("Laura"));
-        school1.addPerson(new Teacher("Emeline", "physique"));
-        school1.addPerson(new Student("Rose", "4e"));
+        school1.addPerson(new Teacher("Emeline", "physique", quatrieme));
+
+        school1.addPerson(new Student("Rose", quatrieme));
+        school1.addPerson(new Student("Paul", cinquieme));
+        school1.addPerson(new Student("Romain", quatrieme));
         school1.ringBell();
+        school1.getPersonByName("Jacques");
+        quatrieme.getPersonsByGrade();
+
     }
 
 }
