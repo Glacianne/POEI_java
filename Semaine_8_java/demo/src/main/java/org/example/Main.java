60bc755c0167f7205c733d11404513ad4d494f8c
Anne-Isabelle <anne.isabelle.rene@gmail.com>
2022-12-16 10:24:43 +0100

    ajout du tp1

---
+++ Semaine_8_java/demo/src/main/java/org/example/Main.java
@@ -0,0 +1,87 @@
+package org.example;
+
+import org.example.model.Personne;
+import org.example.model.PersonneWithPK;
+
+import javax.persistence.*;
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) {
+
+        System.out.println("Hello world!");
+
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("Demojpa");
+        EntityManager em = emf.createEntityManager();
+
+        EntityTransaction transac = em.getTransaction();
+
+        transac.begin();
+        //Création de Personne
+        Personne nouvellePersonne;
+        //pas de set de l'ID
+        for(int i= 0; i<=20; i++){
+            nouvellePersonne = new Personne();
+            nouvellePersonne.setNom("nom"+(i+1));
+            nouvellePersonne.setPrenom("prenom"+(i+1));
+            em.persist(nouvellePersonne);
+            transac.commit();
+            transac.begin();
+            nouvellePersonne = null;
+        }
+
+
+
+        PersonneWithPK personneWithPK = new PersonneWithPK();
+        personneWithPK.setNom("titi");
+        personneWithPK.setPrenom("tutu");
+        em.merge(personneWithPK);
+        transac.commit();
+
+        // Récuperation de personne avec find et getReference
+        transac.begin();
+
+        Personne personne = em.find(Personne.class,1);
+        System.out.println("Personne avec l'ID 1 (find) : "+personne.getNom()+" "+personne.getPrenom());
+
+        Personne personne1 = em.getReference(Personne.class,1);
+        System.out.println("Personne avec l'ID 1 (getReference) : "+personne1.getNom()+" "+personne1.getPrenom());
+
+
+        transac.commit();
+
+        // Reccuperation avec Query
+
+        //createQuery single result
+        Query query = em.createQuery("select p from Personne p where p.nom='nom12'");
+        Personne personne2 =(Personne) query.getSingleResult();
+        System.out.println("Personne avec le nom = nom12 a pour prenom "+personne2.getPrenom()+" et l'id : "+personne2.getId());
+
+
+        //createQuery result list
+        System.out.println("Liste des personnes avec l'id supérieur à 5");
+        Query query1=em.createQuery("select  p from Personne p where p.id > 5");
+        List noms = query1.getResultList();
+        //on recupere une liste d'objet
+        for(Object nom : noms){
+            Personne tmp =(Personne) nom;
+            System.out.println("nom = "+tmp.getNom());
+        }
+
+        // Utilisation du setParameter
+        System.out.println("Liste de personne avec id superieur au parametre set");
+        Query query2=em.createQuery("select p from Personne p where p.id > :id");
+        query2.setParameter("id",15);
+        List noms2 = query2.getResultList();
+        //on recupere une liste d'objet
+        for(Object nom : noms2){
+            Personne tmp =(Personne) nom;
+            System.out.println("nom = "+tmp.getNom());
+        }
+
+        //Modification
+
+        em.close();
+        emf.close();
+    }
+}
--- Semaine_8_java/demo/src/main/java/org/example/Test.java
+++ Semaine_8_java/demo/src/main/java/org/example/Test.java
@@ -19,6 +19,7 @@ public class Test {
 
         trx.begin();
 
+        //Partie 1
         Produit p1 = new Produit("addidas", "chaussures", new Date(), 50.00, 5);
         Produit p2 = new Produit("lacoste", "polo", new Date(), 70.00, 3);
         Produit p3 = new Produit("zara", "legging", new Date(), 15.50, 20);
@@ -31,8 +32,24 @@ public class Test {
         em.persist(p4);
         em.persist(p5);
 
-
         trx.commit();
+
+        //Partie 2
+        Produit pFind2 = em.find(Produit.class, 2);
+        System.out.println("Produit : " + pFind2.getReference() + ", " + pFind2.getMarque() + ", " + pFind2.getPrix() + "euros" );
+
+        //Partie 3
+        Produit pFind3 = em.find(Produit.class, 3);
+        em.remove(pFind3);
+
+        //Partie 4
+        trx.begin();
+        Produit pFind1 = em.find(Produit.class, 1);
+        System.out.println("Ancien prix : " + pFind1.getPrix() + ", " + "Ancien stock : " + pFind1.getStock());
+        pFind1.setPrix(100.00);
+        pFind1.setStock(15);
+        trx.commit();
+        System.out.println("Nouveau prix : " + pFind1.getPrix() + ", " + "Nouveau stock : " + pFind1.getStock());
 
         em.close();
         emf.close();
